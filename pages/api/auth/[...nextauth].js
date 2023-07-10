5a79746668e0e2339a2e118f6b5c4d01bfac8b03
Damien Alves <alves.d24@laposte.net>
2023-07-10 14:50:29 +0200

    feat(global): add database to authentication

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,13 @@
 // pages/api/auth/[...nextauth].js
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     // OAuth authentication providers
     GoogleProvider({
@@ -12,7 +17,6 @@ export default NextAuth({
   ],
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
-      console.log(user);
       return true;
     },
   },
