c05daf030d4f0830de01efce76e9cd2b90b5a00d
Damien Alves <alves.d24@laposte.net>
2023-07-10 14:28:19 +0200

    refactor(SignIn): change component name

---
+++ components/SignIn/index.jsx
@@ -0,0 +1,20 @@
+import styles from "../../styles/Form.module.scss";
+import { useSession, signIn, signOut } from "next-auth/react";
+
+function Form() {
+  const { data: session } = useSession();
+
+  return (
+    <div className={styles.containerForm}>
+      {session ? (
+        <button onClick={() => signOut()}>Sign Out</button>
+      ) : (
+        <button onClick={() => signIn(undefined, { callbackUrl: "/workout" })}>
+          Sign in
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default Form;
