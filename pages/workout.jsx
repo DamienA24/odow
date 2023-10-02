17a2e0945308b254c31fafa281967f43f0e09745
Damien Alves <alves.d24@laposte.net>
2023-10-02 10:18:43 +0200

    refactor(workout): add loading when check authenticate

--- pages/workout.jsx
+++ pages/workout.jsx
@@ -1,11 +1,13 @@
+import { useState, useEffect } from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Workout() {
-  const { data: session } = useSession();
-
+  const { data: session, status } = useSession();
   return (
     <div>
-      {session ? (
+      {status === "loading" ? (
+        <p style={{ color: "red" }}>loading</p>
+      ) : session ? (
         <p style={{ color: "red" }}>workout soon</p>
       ) : (
         <p style={{ color: "red" }}>you need to log in</p>
