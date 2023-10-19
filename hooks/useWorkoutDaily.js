eb5e4a28b7ad721e58d6e4a6570edc10f669e7ab
Damien Alves <alves.d24@laposte.net>
2023-10-19 11:36:31 +0200

    refactor(hooks): remove logs

--- hooks/useWorkoutDaily.js
+++ hooks/useWorkoutDaily.js
@@ -5,13 +5,10 @@ import useCurrentDate from "./useCurrentDate";
 
 function useWorkoutDaily() {
   const date = useCurrentDate();
-  console.log(date);
   const { data, error, isLoading } = useSWR(
     `/api/wod/daily?date=${date}`,
     fetcher
   );
-
-  console.log(data);
 
   return {
     workout: data,
