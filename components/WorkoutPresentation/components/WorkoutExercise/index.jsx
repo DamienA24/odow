c8e8a27a9bcd7272ec6251cb5b53c8f20660291c
Damien Alves <alves.d24@laposte.net>
2023-10-23 09:53:43 +0200

    feat(components): add workoutPresentation component

---
+++ components/WorkoutPresentation/components/WorkoutExercise/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import styles from "./styles/WorkoutExercise.module.scss";
+
+export default function WorkoutExercise() {
+  return (
+    <div className={styles.containerWorkoutExercise}>
+      <img
+        loading="lazy"
+        srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/1aea4a4a-2d31-46ea-87cd-ce5f1086a781?apiKey=cb89b474c6e14831a9cf8eb332a06fd8&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/1aea4a4a-2d31-46ea-87cd-ce5f1086a781?apiKey=cb89b474c6e14831a9cf8eb332a06fd8&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/1aea4a4a-2d31-46ea-87cd-ce5f1086a781?apiKey=cb89b474c6e14831a9cf8eb332a06fd8&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/1aea4a4a-2d31-46ea-87cd-ce5f1086a781?apiKey=cb89b474c6e14831a9cf8eb332a06fd8&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/1aea4a4a-2d31-46ea-87cd-ce5f1086a781?apiKey=cb89b474c6e14831a9cf8eb332a06fd8&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/1aea4a4a-2d31-46ea-87cd-ce5f1086a781?apiKey=cb89b474c6e14831a9cf8eb332a06fd8&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/1aea4a4a-2d31-46ea-87cd-ce5f1086a781?apiKey=cb89b474c6e14831a9cf8eb332a06fd8&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/1aea4a4a-2d31-46ea-87cd-ce5f1086a781?apiKey=cb89b474c6e14831a9cf8eb332a06fd8&"
+        className="exercise-image"
+        alt="Exercise"
+      />
+      <div className={styles.containerExerciseDetails}>
+        <p className={styles.exerciseName}>PUSH-UP</p>
+        <p className={styles.exerciseReps}>10 reps</p>
+      </div>
+    </div>
+  );
+}
