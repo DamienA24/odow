23706de5e67e1e527b3e832ea6b0c51b603563af
Damien Alves <alves.d24@laposte.net>
2023-10-24 10:11:54 +0200

    feat(workoutDate): recover date workout

--- components/WorkoutDate/index.jsx
+++ components/WorkoutDate/index.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 
+import useWorkoutDetailsStore from "../../stores/useWorkoutDetailsStore";
 import styles from "./styles/WorkoutDate.module.scss";
 
 export default function WorkoutDate({ props }) {
+  const { workoutDetails } = useWorkoutDetailsStore();
+  const { date } = workoutDetails;
+  const dateObj = new Date(date);
+
+  const day = String(dateObj.getDate()).padStart(2, "0");
+  const month = String(dateObj.getMonth() + 1).padStart(2, "0");
+  const year = String(dateObj.getFullYear());
+
+  const formattedDate = `${day}-${month}-${year}`;
   return (
     <div className={styles.containerWorkoutDate}>
       <h1>Workout</h1>
-      <p>03/03/2023</p>
+      <p>{formattedDate}</p>
     </div>
   );
 }
