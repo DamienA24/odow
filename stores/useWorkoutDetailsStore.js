d543cfbe41737d6e1d654ecdc146d0696e96d384
Damien Alves <alves.d24@laposte.net>
2023-10-24 10:10:38 +0200

    feat(global): add store management with zustand

---
+++ stores/useWorkoutDetailsStore.js
@@ -0,0 +1,58 @@
+import { create } from "zustand";
+
+const initialWorkoutDetails = () => ({
+  id: null,
+  name: "",
+  durationInMinutes: null,
+  description: "",
+  BodyParts: {
+    partName: "",
+  },
+  date: "",
+  WorkoutRounds: [
+    {
+      roundId: "",
+      Rounds: [
+        {
+          exerciseId: null,
+          Exercises: {
+            name: "",
+            description: "",
+            urlVideo: "",
+            DifficultyLevels: {
+              levelName: "",
+            },
+            ExerciseBodyParts: [
+              {
+                BodyParts: {
+                  partName: "",
+                },
+              },
+              {
+                BodyParts: {
+                  partName: "",
+                },
+              },
+              {
+                BodyParts: {
+                  partName: "",
+                },
+              },
+            ],
+          },
+          reps: null,
+          rest: null,
+          order: null,
+        },
+      ],
+    },
+  ],
+});
+
+const useWorkoutDetailsStore = create((set) => ({
+  workoutDetails: initialWorkoutDetails(),
+  updateWorkoutDetails: (newWorkoutDetails) =>
+    set({ workoutDetails: newWorkoutDetails }),
+}));
+
+export default useWorkoutDetailsStore;
