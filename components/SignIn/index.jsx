d30c45117f076153632ee512d082c292e5346a89
Damien Alves <alves.d24@laposte.net>
2023-10-26 14:09:37 +0200

    refactor(signin): redirect if logged

--- components/SignIn/index.jsx
+++ components/SignIn/index.jsx
@@ -1,12 +1,20 @@
 import styles from "./styles/Form.module.scss";
+
 import { useSession, signIn, signOut } from "next-auth/react";
+import { useRouter } from "next/router";
 
 function Form() {
+  const router = useRouter();
   const { data: session } = useSession();
+
+  if (session) {
+    router.push("/workout");
+  }
 
   return (
     <div className={styles.containerForm}>
       {session ? (
+        /*//TODO CREATE COMPONENT BUTTON SIGNOUT* */
         <button onClick={() => signOut()}>Sign Out</button>
       ) : (
         <button onClick={() => signIn(undefined, { callbackUrl: "/workout" })}>
