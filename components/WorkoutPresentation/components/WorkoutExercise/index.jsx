0347403420ccb13e9dccc3c535273a2e58370d36
Damien Alves <alves.d24@laposte.net>
2023-10-26 15:01:44 +0200

    feat(components): add modal display exercise

--- components/WorkoutPresentation/components/WorkoutExercise/index.jsx
+++ components/WorkoutPresentation/components/WorkoutExercise/index.jsx
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Image from "next/image";
+
+import ExerciseModal from "./components/ExerciseModal";
 
 import styles from "./styles/WorkoutExercise.module.scss";
 
-export default function WorkoutExercise({ name, reps }) {
+export default function WorkoutExercise({
+  name,
+  reps,
+  stickerVideo,
+  urlVideo,
+  description,
+}) {
+  const [isModalOpen, setModalOpen] = useState(false);
+
   return (
     <div className={styles.containerWorkoutExercise}>
       <img
         loading="lazy"
-        srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/1aea4a4a-2d31-46ea-87cd-ce5f1086a781?apiKey=cb89b474c6e14831a9cf8eb332a06fd8&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/1aea4a4a-2d31-46ea-87cd-ce5f1086a781?apiKey=cb89b474c6e14831a9cf8eb332a06fd8&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/1aea4a4a-2d31-46ea-87cd-ce5f1086a781?apiKey=cb89b474c6e14831a9cf8eb332a06fd8&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/1aea4a4a-2d31-46ea-87cd-ce5f1086a781?apiKey=cb89b474c6e14831a9cf8eb332a06fd8&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/1aea4a4a-2d31-46ea-87cd-ce5f1086a781?apiKey=cb89b474c6e14831a9cf8eb332a06fd8&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/1aea4a4a-2d31-46ea-87cd-ce5f1086a781?apiKey=cb89b474c6e14831a9cf8eb332a06fd8&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/1aea4a4a-2d31-46ea-87cd-ce5f1086a781?apiKey=cb89b474c6e14831a9cf8eb332a06fd8&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/1aea4a4a-2d31-46ea-87cd-ce5f1086a781?apiKey=cb89b474c6e14831a9cf8eb332a06fd8&"
-        className="exercise-image"
+        srcSet={stickerVideo}
         alt="Exercise"
+        onClick={() => setModalOpen(true)}
+        className={styles.imageYoutube}
       />
       <div className={styles.containerExerciseDetails}>
         <p className={styles.exerciseName}>{name}</p>
         <p className={styles.exerciseReps}>{reps} reps</p>
       </div>
+      {isModalOpen ? (
+        <ExerciseModal
+          isOpen={isModalOpen}
+          onClose={() => setModalOpen(false)}
+          exercise={{ name, reps, urlVideo, description }}
+        />
+      ) : (
+        ""
+      )}
     </div>
   );
 }
