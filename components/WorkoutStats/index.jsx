66e3ca2dd904a6da02da0cae5951774964d3a64e
Damien Alves <alves.d24@laposte.net>
2023-10-26 15:09:13 +0200

    refactor(style): change heart icon to use

--- components/WorkoutStats/index.jsx
+++ components/WorkoutStats/index.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
+import Image from "next/image";
+
 import useWorkoutDetailsStore from "../../stores/useWorkoutDetailsStore";
 import styles from "./styles/WorkoutStats.module.scss";
+import heart from "../../public/icons/heart.svg";
 
 export default function WorkoutStats({ props }) {
   const { workoutDetails } = useWorkoutDetailsStore();
@@ -18,12 +21,7 @@ export default function WorkoutStats({ props }) {
         <p className={styles.statValue}>{calories}</p>
         <p className={styles.statLabel}>Calories</p>
       </div>
-      <img
-        loading="lazy"
-        srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/e1237757-7c9c-4a1b-bed9-694121cc6c14?apiKey=cb89b474c6e14831a9cf8eb332a06fd8&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/e1237757-7c9c-4a1b-bed9-694121cc6c14?apiKey=cb89b474c6e14831a9cf8eb332a06fd8&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/e1237757-7c9c-4a1b-bed9-694121cc6c14?apiKey=cb89b474c6e14831a9cf8eb332a06fd8&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/e1237757-7c9c-4a1b-bed9-694121cc6c14?apiKey=cb89b474c6e14831a9cf8eb332a06fd8&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/e1237757-7c9c-4a1b-bed9-694121cc6c14?apiKey=cb89b474c6e14831a9cf8eb332a06fd8&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/e1237757-7c9c-4a1b-bed9-694121cc6c14?apiKey=cb89b474c6e14831a9cf8eb332a06fd8&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/e1237757-7c9c-4a1b-bed9-694121cc6c14?apiKey=cb89b474c6e14831a9cf8eb332a06fd8&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/e1237757-7c9c-4a1b-bed9-694121cc6c14?apiKey=cb89b474c6e14831a9cf8eb332a06fd8&"
-        className={styles.statImage}
-        alt="Workout"
-      />
+      <Image src={heart} alt="heart" className={styles.statImage} />
     </div>
   );
 }
