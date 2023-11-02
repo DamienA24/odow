ea1ce28d8c31e3fc55a9e1fe830abd3eb62a78a3
Damien Alves <alves.d24@laposte.net>
2023-11-02 09:17:55 +0100

    feat(ButtonStart): start session

--- components/ButtonStart/index.jsx
+++ components/ButtonStart/index.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 
+import { useSession } from "next-auth/react";
 import Image from "next/image";
-import playButton from "./images/play-button.svg";
+
+import useWorkoutDetailsStore from "stores/useWorkoutDetailsStore";
 import styles from "./styles/ButtonStart.module.scss";
+import playButton from "./images/play-button.svg";
+import useApiRequest from "hooks/useApiRequest";
 
 export default function ButtonStart() {
+  const { data: session } = useSession();
+  const { data, error, isLoading, request } = useApiRequest();
+  const { workoutDetails } = useWorkoutDetailsStore();
+
+  //TODO MANAGE IF SESSION NOT STARTED, ERROR OR WHATEVER
+  const handleStartClick = () => {
+    request("/api/wod/session", "POST", {
+      email: session.user.email,
+      workoutId: workoutDetails.id,
+    });
+  };
   return (
-    <div className={styles.containerButtonStart}>
+    <div className={styles.containerButtonStart} onClick={handleStartClick}>
       <Image src={playButton} alt="icon play" />
     </div>
   );
